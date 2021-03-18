44ece289df5474bce0020fcdb0de692bb8a06a58
Willow Lane <lanw5377@gmail.com>
2021-03-18 14:43:39 -0400

    analyse - username

---
+++ analyze.py
@@ -0,0 +1,8 @@
+print("welcome to the text analysist tool, will mine and analyze a body of text from a file you give me")
+# Get Username
+
+#Print message prompting user to input their name
+Username = input("\n To beging, please enter your username:\n")
+
+#Greet the user
+print("Hello, " + Username)
