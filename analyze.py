a2e046a84572f2d050f100471fcd45027c1ed312
Willow Lane <lanw5377@gmail.com>
2021-03-23 10:33:58 -0400

    updated test analysist

--- analyze.py
+++ analyze.py
@@ -6,16 +6,26 @@ def welcomeUser():
 def getUsername():  
     #Print message prompting user to input their name
     usernameFromInput = input("\n To beging, please enter your username:\n")
+    
+    usernameLessThan5Chars = len(usernameFromInput) < 5
+    print("Less than 5 characters: " + str(usernameLessThan5Chars))
+
+    usernameIsInvalid = usernameLessThan5Chars
+     
+    if usernameIsInvalid:
+        print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces, and cannot start with a number!")
+        usernameFromInput = "User123"
+
     return usernameFromInput
 
 # Greet the User
 def greetUser(name): 
     print("Hello, " + name)
 
-def runProgram():
+
     welcomeUser() 
     username = getUsername()
     greetUser(username)
 
-runProgram()
+
 
