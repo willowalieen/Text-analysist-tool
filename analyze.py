bfc03e9653733f1672f11518a81201e45467987b
Willow Lane <lanw5377@gmail.com>
2021-03-23 12:22:09 -0400

    username updated

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+
+
 #Welcome User
 def welcomeUser():
     print("\nWelcome to the text analysist tool, will mine and analyze a body of text from a file you give me")
@@ -6,15 +8,11 @@ def welcomeUser():
 def getUsername():  
     #Print message prompting user to input their name
     usernameFromInput = input("\n To beging, please enter your username:\n")
-    
-    usernameLessThan5Chars = len(usernameFromInput) < 5
-    print("Less than 5 characters: " + str(usernameLessThan5Chars))
-
-    usernameIsInvalid = usernameLessThan5Chars
-     
-    if usernameIsInvalid:
+         
+    if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
         print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces, and cannot start with a number!")
         usernameFromInput = "User123"
+        print("Assigning new username: " + usernameFromInput)
 
     return usernameFromInput
 
@@ -23,9 +21,9 @@ def greetUser(name):
     print("Hello, " + name)
 
 
-    welcomeUser() 
-    username = getUsername()
-    greetUser(username)
+welcomeUser() 
+username = getUsername()
+greetUser(username)
 
 
 
