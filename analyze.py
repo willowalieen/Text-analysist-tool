5b3a71c1b3dc1cf348e3527c981b36dc92302cf4
Willow Lane <lanw5377@gmail.com>
2021-06-25 08:56:19 -0300

    loops

--- analyze.py
+++ analyze.py
@@ -6,16 +6,24 @@ def welcomeUser():
 
 # Get Username
 def getUsername():  
-    #Print message prompting user to input their name
-    usernameFromInput = input("\n To beging, please enter your username:\n")
-         
-    if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
-        print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces, and cannot start with a number!")
-        usernameFromInput = "User123"
-        print("Assigning new username: " + usernameFromInput)
 
-    return usernameFromInput
+    MaxAttempts = 3
+    attempts = 0
 
+    while attempts < MaxAttempts:
+        #Print message prompting user to input their name
+        usernameFromInput = input("\n To beging, please enter your username:\n")
+       
+        if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
+            print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces, and cannot start with a number!")
+        else: 
+            return usernameFromInput
+        
+        attempts += 1
+
+    print("Assigning username instead...")
+    return generate_username()[0]       
+        
 # Greet the User
 def greetUser(name): 
     print("Hello, " + name)
